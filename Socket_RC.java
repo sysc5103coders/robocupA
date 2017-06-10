52fab31c2884df4d262c0ee2c2e37c61765ebd59
GeraMantecon <lateralus_reflection@hotmail.com>
2017-06-10 14:40:21 -0400

    Single socket approach

---
+++ Socket_RC.java
@@ -0,0 +1,23 @@
+import java.net.*;
+
+public class Socket_RC {
+    
+    public DatagramSocket m_socket;    // Socket to communicate with server
+    private static Socket_RC socket_rc = new Socket_RC();
+    
+    protected Socket_RC(){
+        try{    
+            m_socket = new DatagramSocket();
+        }catch(Exception ex){}
+    }
+    
+    public void finalize()
+    {
+	m_socket.close();
+    }
+    
+    public static Socket_RC getInstance(){
+        return socket_rc;
+    }
+    
+}
